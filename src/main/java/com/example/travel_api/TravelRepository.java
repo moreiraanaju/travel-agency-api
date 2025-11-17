d38cc36aa81fd95f02745e24947a1ae1b9ea6b18
Ana Ju <ferreiraanajussara@gmail.com>
2025-11-17 19:54:43 -0300

    feat: implement search endpoint (name/location) and repository query

--- src/main/java/com/example/travel_api/TravelRepository.java
+++ src/main/java/com/example/travel_api/TravelRepository.java
@@ -1,3 +1,16 @@
+// A Repository is a class responsible for communicating with the DB
+// This is where CRUD operations would normally be defined, but Spring Data provides them automatically by default 
+
 package com.example.travel_api;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelRepository extends JpaRepository<Travel, Long> {   // JpaRepository<domainType, idType>
+
+    List<Travel> findByDestinationNameContainingIgnoreCaseOrLocationContainingIgnoreCase (
+        String destinationName,
+        String location
+    );
+}
 
