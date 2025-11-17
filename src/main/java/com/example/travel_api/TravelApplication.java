ee0b2c3d851ec96fcc5d427e374a5fcca682e1f6
Ana Ju <ferreiraanajussara@gmail.com>
2025-11-17 20:04:19 -0300

    refactor: rename main Spring Boot application class

--- src/main/java/com/example/travel_api/TravelApplication.java
+++ src/main/java/com/example/travel_api/TravelApplication.java
@@ -1,13 +1,15 @@
+// The class in this file is the entry point of the application
+
 package com.example.travel_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelApiApplication {
+public class TravelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravelApiApplication.class, args);
+		SpringApplication.run(TravelApplication.class, args);
 	}
 
 }
